5ce75d250b4f3c6359725a551d530d7b5f57712b
peterhsj <peterhsj@gmail.com>
2024-08-07 14:55:36 +0800

    vite init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const mun = ref(2)
+function clickMe() {
+  num.value++
+}
+return { num, clickMe }
+</script>
+
+<template>
+  <div>
+    {{ num }}
+    <button type="button" @click="clickMe">點我</button>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//console.log({ vue })
+
+createApp(App).mount('#app')
